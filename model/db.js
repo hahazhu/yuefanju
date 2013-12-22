64f79d8ded79eb1777630ccad3b6f9b444f45df5
hahazhu <191191308@qq.com>
2013-12-22 21:54:54 +0800

    数据库连接池

---
+++ model/db.js
@@ -0,0 +1,7 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'hahazhu',
+  password : 'yuefanju.1',
+  database : 'yuefanju'
+});
