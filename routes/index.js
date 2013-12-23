a895f4301bacd79cee18e3f9cae141ee6581cead
Administrator <Administrator@SXASITUAAP8PU1L>
2013-12-24 00:48:30 +0800

    首页跳转到搜索结果页面，通过post传递参数并通过后台查询得到结果返回到页面

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,32 @@
  * GET home page.
  */
 module.exports = function(app) {
+	app.post('/post', function(req, res) {
+		res.redirect('/searchRst/' + req.body.searchText);
+	});
 	app.get('/', function(req, res) {
 		res.render('index', {
-			title: 'Express'
+			title : 'Express'
 		})
 	});
-	app.get('/searchRst', function(req, res) {
-		res.render('searchRst', {
-			title: 'Express'
-		})
+	app.get('/searchRst/:searchText', function(req, res) {
+		var pool = require('../model/db').pool;
+		pool.getConnection(function(err, conn) {
+			conn.query('select t.host,t.db,t.user from mysql.db t', function(
+					err, rows, fields) {
+				if (err)
+					throw err;
+				console.log('The first host is: ', rows[0].host);
+				console.log('The length is: ', rows.length);
+				res.render('searchRst', {
+					searchWord : req.params.searchText,
+					rstLength : rows.length,
+					rowRst : rows
+				})
+			});
+		});
 	});
-	app.get('/newEvent',function(req,res){
+	app.get('/newEvent', function(req, res) {
 		res.render('newEvent')
 	})
 }
