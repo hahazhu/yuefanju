2cbedb720c2f363ff8dd9fbd823c1e731bd464d9
gaoq <Administrator@GaoLenovoZ>
2013-12-29 16:27:00 +0800

    结果高亮显示而且可以用空格隔开搜索关键字

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 /*
  * GET home page.
  */
+//未输入搜索内容
+var search_none ='select t.event_name,t.event_creator,t.event_crtime,t.event_comment '+
+				'from dat_event_info t order by t.event_crtime desc';
+//输入搜索内容，但是未选择类别
+var search_all ='select t.event_name,t.event_creator,t.event_crtime,t.event_comment  '+
+				'from dat_event_info t where t.event_name REGEXP ? or t.event_creator REGEXP ? or t.event_comment REGEXP ?'+
+				'order by t.event_crtime desc';
 module.exports = function(app) {
 	app.post('/searchRst', function(req, res) {
 		console.log('The length is: ', req.body.searchText);
@@ -15,7 +22,7 @@ module.exports = function(app) {
 	app.get('/searchRst/', function(req, res) {
 		var pool = require('../model/db').pool;
 		pool.getConnection(function(err, conn) {
-			conn.query('select t.event_name,t.event_creator,t.event_crtime,t.event_comment from dat_event_info t order by t.event_crtime desc', function(
+			conn.query(search_none, function(
 					err, rows, fields) {
 				if (err)
 					throw err;
@@ -31,9 +38,13 @@ module.exports = function(app) {
 	});
 	app.get('/searchRst/:searchText', function(req, res) {
 		var pool = require('../model/db').pool;
-		var values = ['%'+req.params.searchText+'%','%'+req.params.searchText+'%','%'+req.params.searchText+'%']
+		
+        var r = /\s+/g;
+        var searchTextR= req.params.searchText.replace(r, '|');
+        
+		var values = [''+searchTextR+'',''+searchTextR+'',''+searchTextR+'']
 		pool.getConnection(function(err, conn) {
-			conn.query('select t.event_name,t.event_creator,t.event_crtime,t.event_comment from dat_event_info t where t.event_name like ? or t.event_creator like ? or t.event_comment like ?',values, function(
+			conn.query(search_all,values, function(
 					err, rows, fields) {
 				if (err)
 					throw err;
