1920951ca1efab586e209f453c3a4dcd6bc81d9a
hahazhu <191191308@qq.com>
2013-12-29 23:05:25 +0800

    改表结构

--- db_script/tab/tab_dat_event_info_20131224_gaoq.sql
+++ db_script/tab/tab_dat_event_info_20131224_gaoq.sql
@@ -1,10 +1,8 @@
-CREATE TABLE `yuefanju`.`dat_event_info` (
-  `event_id` VARCHAR(10) NOT NULL COMMENT '活动ID',
-  `event_name` VARCHAR(100) COMMENT '活动名称',
-  `event_creator` VARCHAR(10) COMMENT '活动创建者',
-  `event_crtime` DATETIME COMMENT '活动创建时间',
-  `event_comment` VARCHAR(4000) COMMENT '活动简介',
+CREATE TABLE `dat_event_info` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '活动ID',
+  `event_name` varchar(100) DEFAULT NULL COMMENT '活动名称',
+  `event_creator` varchar(10) DEFAULT NULL COMMENT '活动创建者',
+  `event_crtime` varchar(8) DEFAULT NULL COMMENT '活动时间',
+  `event_comment` varchar(4000) DEFAULT NULL COMMENT '活动简介',
   PRIMARY KEY (`event_id`)
-)
-ENGINE = InnoDB
-COMMENT = '活动基本信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='活动基本信息表';
