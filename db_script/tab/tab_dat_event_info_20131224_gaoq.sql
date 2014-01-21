8c0b98024135b3c5925bf6eaf6bc8d4e77255f45
Administrator <Administrator@GaoLenovoZ>
2014-01-21 23:17:36 +0800

    重新提表结构

--- db_script/tab/tab_dat_event_info_20131224_gaoq.sql
+++ db_script/tab/tab_dat_event_info_20131224_gaoq.sql
@@ -1,7 +1,10 @@
-CREATE TABLE `dat_event_info` (
-  `event_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '活动ID',
+DROP TABLE IF EXISTS `yuefanju`.`dat_event_info`;
+CREATE TABLE  `yuefanju`.`dat_event_info` (
+  `event_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '活动ID',
   `event_name` varchar(100) DEFAULT NULL COMMENT '活动名称',
   `event_creator` varchar(10) DEFAULT NULL COMMENT '活动创建者',
+  `event_ispwd` varchar(2) DEFAULT NULL COMMENT '是否加密（0：否 1：是）',
+  `event_password` varchar(32) DEFAULT NULL COMMENT '活动密码',
   `event_crtime` varchar(8) DEFAULT NULL COMMENT '活动时间',
   `event_comment` varchar(4000) DEFAULT NULL COMMENT '活动简介',
   PRIMARY KEY (`event_id`)
