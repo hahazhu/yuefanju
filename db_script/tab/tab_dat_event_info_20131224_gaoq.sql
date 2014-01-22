6b810c79888395fe8191eb89ac82304cfc7ef0fb
Administrator <Administrator@GaoLenovoZ>
2014-01-22 23:02:41 +0800

    昨天提错了

--- db_script/tab/tab_dat_event_info_20131224_gaoq.sql
+++ db_script/tab/tab_dat_event_info_20131224_gaoq.sql
@@ -5,7 +5,7 @@ CREATE TABLE  `yuefanju`.`dat_event_info` (
   `event_creator` varchar(10) DEFAULT NULL COMMENT '活动创建者',
   `event_ispwd` varchar(2) DEFAULT NULL COMMENT '是否加密（0：否 1：是）',
   `event_password` varchar(32) DEFAULT NULL COMMENT '活动密码',
-  `event_crtime` varchar(8) DEFAULT NULL COMMENT '活动时间',
+  `event_crtime` datetime DEFAULT NULL COMMENT '活动时间',
   `event_comment` varchar(4000) DEFAULT NULL COMMENT '活动简介',
   PRIMARY KEY (`event_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='活动基本信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='活动基本信息表';
