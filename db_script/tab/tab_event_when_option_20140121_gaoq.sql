e3d07b1ab0d5c77496d5400e3c073373e15f37b3
hahazhu <191191308@qq.com>
2014-01-25 22:52:05 +0800

    改主键

--- db_script/tab/tab_event_when_option_20140121_gaoq.sql
+++ db_script/tab/tab_event_when_option_20140121_gaoq.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS `yuefanju`.`event_when_option`;
-CREATE TABLE  `yuefanju`.`event_when_option` (
+CREATE TABLE `event_when_option` (
   `event_id` int(10) unsigned NOT NULL COMMENT '活动id',
-  `when_seq` varchar(5) CHARACTER SET latin1 NOT NULL COMMENT '时间选项序号',
+  `when_seq` int(11) NOT NULL AUTO_INCREMENT COMMENT '时间选项序号',
   `when_describe` varchar(100) CHARACTER SET latin1 DEFAULT NULL COMMENT '时间描述',
-  PRIMARY KEY (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='活动时间选项表';
+  PRIMARY KEY (`when_seq`,`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8 COMMENT='活动时间选项表';
